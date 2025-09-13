48d8329833eef96c33ab0f672ca944fd9605498b
Rohyit <rathodr070@gmail.com>
2025-09-13 23:14:04 +0530

    Solved Patients With a Condition Problem

---
+++ Advanced_String_Functions_Regex_Clause/Patients With a Condition.sql
@@ -0,0 +1,4 @@
+-- Q. https://leetcode.com/problems/patients-with-a-condition/
+
+select patient_id, patient_name, conditions from patients 
+where conditions like 'DIAB1%' or conditions like '% DIAB1%';
