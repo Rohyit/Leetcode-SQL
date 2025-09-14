5679df0a0ea2f05acc27a78adf251328a1f907d1
Rohyit <rathodr070@gmail.com>
2025-09-14 13:20:04 +0530

    Solved Employees Whose Manager Left the Company Query

---
+++ Subqueries/Employees Whose Manager Left the Company.sql
@@ -0,0 +1,5 @@
+-- Q. https://leetcode.com/problems/employees-whose-manager-left-the-company/
+
+select employee_id from employees 
+where salary < 30000 and manager_id not in (select employee_id from employees) 
+order by employee_id;
