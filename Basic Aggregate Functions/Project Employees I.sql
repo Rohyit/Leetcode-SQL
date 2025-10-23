b34a6a86a9d5e2c38ed0f4a5d05f96c634a1205a
Rohyit <rathodr070@gmail.com>
2025-10-23 11:54:16 +0530

    Solved Project Employees I query

--- Basic Aggregate Functions/Project Employees I.sql
+++ Basic Aggregate Functions/Project Employees I.sql
@@ -1,5 +1,6 @@
 -- Q. https://leetcode.com/problems/project-employees-i/
 
-select project_id, round(avg(experience_years), 2) as "average_years" from project, employee
+select project_id, round(avg(experience_years), 2) as "average_years" 
+from project, employee
 where project.employee_id = employee.employee_id
 group by project_id;
