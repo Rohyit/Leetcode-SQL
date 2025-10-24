b4152b489b00151fb99a0eec3b8eef76399f8d2c
Rohyit <rathodr070@gmail.com>
2025-10-24 22:26:22 +0530

    Solved Number of Unique Subjects Taught by Each Teacher query

--- Sorting and Grouping/Number of Unique Subjects Taught by Each Teacher.sql
+++ Sorting and Grouping/Number of Unique Subjects Taught by Each Teacher.sql
@@ -1,4 +1,5 @@
 -- Q. https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/
 
-select teacher_id, count(distinct subject_id) as "cnt" from teacher
+select teacher_id, count(distinct subject_id) as "cnt" 
+from teacher
 group by teacher_id;
