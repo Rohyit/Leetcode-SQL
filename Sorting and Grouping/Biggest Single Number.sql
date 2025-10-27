02d9f9214f5e2bf9b9ef4f479f0b00f69057dedf
Rohyit <rathodr070@gmail.com>
2025-10-27 17:20:01 +0530

    Solved Biggest Single Number query

--- Sorting and Grouping/Biggest Single Number.sql
+++ Sorting and Grouping/Biggest Single Number.sql
@@ -1,5 +1,6 @@
 -- Q. https://leetcode.com/problems/biggest-single-number/
 
-select max(num) as "num" from (select num from mynumbers 
+select max(num) as "num" 
+from (select num from mynumbers 
 group by num
 having count(num) = 1) as abcd;
